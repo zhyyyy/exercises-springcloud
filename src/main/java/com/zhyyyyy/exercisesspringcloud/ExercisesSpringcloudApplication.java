cc712dd580575429f997ba07092f24d2710464d0
zhaohongyang <macrocean@foxmail.com>
2018-08-29 20:00:21 +0800

    frist push

---
+++ src/main/java/com/zhyyyyy/exercisesspringcloud/ExercisesSpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.zhyyyyy.exercisesspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesSpringcloudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhyyyyy/exercisesspringcloud/ExercisesSpringcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhyyyyy.exercisesspringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExercisesSpringcloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
