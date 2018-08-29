c8e88d3e229eb9fe58722c95b10744f36790f9a0
zhaohongyang <macrocean@foxmail.com>
2018-08-29 20:52:49 +0800

    @EnableEurekaServer

--- src/main/java/com/zhyyyyy/exercisesspringcloud/ExercisesSpringcloudApplication.java
+++ src/main/java/com/zhyyyyy/exercisesspringcloud/ExercisesSpringcloudApplication.java
@@ -2,11 +2,13 @@ package com.zhyyyyy.exercisesspringcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ExercisesSpringcloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExercisesSpringcloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExercisesSpringcloudApplication.class, args);
+    }
 }
